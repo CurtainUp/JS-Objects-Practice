2cce426dd3788d9fd93d80373b3df622daa45373
Elyse Dawson <dawson.elyse@gmail.com>
2018-10-10 19:55:42 -0500

    Did pets and propvalues exercises. Completed first challenge.

---
+++ campaign.js
@@ -0,0 +1,60 @@
+const campaign = {
+  district: "5th Congressional District",
+  platforms: {
+    taxes: "Keep 'em low!",
+    jobs: "Make more of 'em!",
+    infrastructure: "It's good for 'em!",
+    healthcare: "It keeps 'em alive!",
+    crime: "Keep 'em safe!"
+  },
+  donation: "https://www.elizabethsanger.org/moneygrab.html",
+  calendar: [
+    {
+      date: "June 5th, 2018",
+      timeStart: "3PM",
+      timeEnd: "5PM",
+      place: "Park full of babies",
+      eventName: "Kiss 'em All!",
+    },
+    {
+      date: "June 6th, 2018",
+      timeStart: "12PM",
+      timeEnd: "2PM",
+      place: "Hands Convention Center",
+      eventName: "Shake 'em All!",
+    },
+    {
+      date: "June 7th, 2018",
+      timeStart: "10PM",
+      timeEnd: "12AM",
+      place: "Sanger Mansion",
+      eventName: "Victory Party: Red, White, and BOOYAH"
+    }
+  ],
+  volunteers: [
+    {
+      name: "Jason",
+      address: "478 Easy St.",
+      email: "j.sellout@sengerftw.org",
+      phone: "555-555-5556",
+      avail: "Open",
+      tasks: ["canvasing", "coffee runs", "back massages - Elizabeth only"]
+    },
+    {
+      name: "Ja Rule",
+      address: "808 Beats Ave.",
+      email: "j.rulez@sengerftw.org",
+      phone: "555-555-5557",
+      avail: "Weekends",
+      tasks: ["donations", "uninspiring duets", "driving"]
+    }
+  ],
+  biography: "Elizabeth Senger was born to rich people but did really well in school and was in 4H club so she deserves this.",
+  imgGallery: {
+    headshot: "https://i.pinimg.com/originals/ea/cc/9d/eacc9da5e8eaf40204d06edc7cc9a20c.jpg",
+    family: "https://vignette.wikia.nocookie.net/harrypotter/images/a/a8/Inquisitorial.png/revision/latest?cb=20170102190753",
+    constituents: "https://static3.srcdn.com/wordpress/wp-content/uploads/2017/06/Dolores-Umbridge-slaps-Harry-Potter.jpg"
+  },
+  mission: "Keep 'em voting for ya!",
+  voteReg: "https://www.sortinghat.com",
+}
---
+++ pet.js
@@ -0,0 +1,8 @@
+const pet = {
+  name: "Patch",
+  species: "pupper",
+  nicknames: ["Patchy", "Patchaloo", "Punk", "Patchy Elliott"],
+  age: 4
+}
+
+console.log(pet);
